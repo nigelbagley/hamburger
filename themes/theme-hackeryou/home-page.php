f34b0edc953fff1245714e45a51bb98382ede813
nigelbagley <bagley.nigel@gmail.com>
2015-05-28 11:33:20 -0400

    added in flexbox

--- themes/theme-hackeryou/home-page.php
+++ themes/theme-hackeryou/home-page.php
@@ -6,13 +6,27 @@
 
 get_header();  ?>
  	<section class="bannerHeader">
+	 	<div class="headerMain">
+	 		<h1>Hey there, want a burger?</h1>
+	 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At soluta, sequi perferendis dignissimos voluptatum magnam illo aperiam ut, optio beatae!</p>
+	 	</div>	
+	</section>
+	<section class="hamburgerChoices">
+		<div class="hamburger">
+			<img src="img/burger.jpg" alt="">
+			<h1>lorem</h1>
+			<p>Lorem ipsum dolor.</p>
+			<button>Order Now</button>
+		</div>
+		<div class="hamburger">
+			<img src="<?php bloginfo('template_directory') ?>/img/burger.jpg" alt="">
+			<h1>lorem</h1>
+			<p>Lorem ipsum dolor.</p>
+			<button>Order Now</button>
+		</div>
 
- 	<div class="headerMain">
- 		<h1>Hey there, want a burger?</h1>
- 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At soluta, sequi perferendis dignissimos voluptatum magnam illo aperiam ut, optio beatae!</p>
 
- 	</div>
-		
+
 	</section>
 <!-- Banner -->
 <div class="main">
