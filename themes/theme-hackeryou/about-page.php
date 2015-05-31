e094d8bbcb22a199b661e1b57b1f95265acd5735
mausercodes <john_mauser09@hotmail.com>
2015-05-31 18:54:57 -0400

    added media query 945px

--- themes/theme-hackeryou/about-page.php
+++ themes/theme-hackeryou/about-page.php
@@ -10,50 +10,53 @@ get_header();  ?>
 
  	</section>
 
-
 	<div class="wallpaper clearfix">
-	 	<div class="container">
-	 	<h1>About Us:</h1>
-		 	<div class="aboutContainer">
-		 		<div class="aboutUs">
-		 			<i class="fa fa-cutlery"></i>
-		 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, nesciunt!</p>
-		 		</div>
-		 		<div class="aboutUs">
-		 			<i class="fa fa-building-o"></i>
-		 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, nesciunt!</p>
-		 		</div>
+		<div class="wrapper">
+		 	<div class="container">
+		 	<h1>About Us:</h1>
+			 	<div class="aboutContainer">
+			 		<div class="aboutUs">
+			 			<i class="fa fa-cutlery"></i>
+			 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, nesciunt!</p>
+			 		</div>
+			 		<div class="aboutUs">
+			 			<i class="fa fa-building-o"></i>
+			 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, nesciunt!</p>
+			 		</div>
+			 	</div>
+			 	<div class="aboutContainer">
+			 		<div class="aboutUs">
+			 			<i class="fa fa-cutlery"></i>
+			 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, nesciunt!</p>
+			 		</div>
+			 		<div class="aboutUs">
+			 			<i class="fa fa-building-o"></i>
+			 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, nesciunt!</p>
+			 		</div>
+			 	</div>
 		 	</div>
-		 	<div class="aboutContainer2">
-		 		<div class="aboutUs">
-		 			<i class="fa fa-cutlery"></i>
-		 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, nesciunt!</p>
-		 		</div>
-		 		<div class="aboutUs">
-		 			<i class="fa fa-building-o"></i>
-		 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, nesciunt!</p>
-		 		</div>
-		 	</div>
-	 	</div>
+		 </div>
 	 </div>
  	<div class="wallpaper clearfix">
- 	<div class="container">
- 	<div class="founderTitle">
- 	<h1>Founders:</h1>
- 	</div>
- 		<div class="founders">
- 			<img src="http://www.fillmurray.com/200/200" alt="">
- 			<p>Lorem ipsum dolor sit amet.</p>
- 		</div>
- 		<div class="founders">
- 			<img src="http://www.fillmurray.com/200/200" alt="">
- 			<p>Lorem ipsum dolor sit amet.</p>
- 		</div>
- 		<div class="founders">
- 			<img src="http://www.fillmurray.com/200/200" alt="">
- 			<p>Lorem ipsum dolor sit amet.</p>
- 		</div>
- 	</div>
+ 		<div class="wrapper">
+	 		<div class="container">
+	 			<div class="founderTitle">
+	 				<h1>Founders:</h1>
+	 			</div>
+				<div class="founders">
+					<img src="http://www.fillmurray.com/200/200" alt="">
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+				<div class="founders">
+					<img src="http://www.fillmurray.com/200/200" alt="">
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+				<div class="founders">
+					<img src="http://www.fillmurray.com/200/200" alt="">
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+			</div>
+		</div>
 	</div>
 <!-- Banner -->
 <div class="main">
