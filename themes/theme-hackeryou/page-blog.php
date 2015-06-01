45f2430806562ea7222f1f556b3d867965f63abc
nigelbagley <bagley.nigel@gmail.com>
2015-05-31 20:38:25 -0400

    added font awesome fonts to about page and fixed blog page

--- themes/theme-hackeryou/page-blog.php
+++ themes/theme-hackeryou/page-blog.php
@@ -13,19 +13,29 @@ get_header();  ?>
     <div class="wrapper">
   	 <div class="container">
       <?php // Start the loop ?>
+
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-  	 
+  	    
         <h2><?php the_title(); ?></h2>
-        <p><?php the_author_posts_link(); ?><----Click Me, to see what this guys said!</p>
-        <!-- <p>See what he has said!</p> -->
+       <!--  <p><?php the_author_posts_link(); ?><----Click Me, to see what this guys said!</p>
+        <!-- <p>See what he has said!</p> --> 
 
        <?php echo get_avatar( get_the_author_meta( 'ID' ), 190 ); ?>
-        <?php the_content(); ?>
-        
+        <h3>More posts from
+        <a class="name" href="<?php echo get_author_posts_url( get_the_author_meta('ID') ); ?>">
+          <?php the_author(); ?>
+        </a>
+      </h3>
+        <?php get_the_content(); ?>
+       
+
+      
 
 
 
       <?php endwhile; // end the loop?>
+
+
     </div> <!-- /.container -->
   </div> <!-- /.main -->
 </div>
